393f0af506d8aab464f35890b0471d4ac6e7875f
SR-NANDINI <srnandini2006@gmail.com>
2026-07-26 09:46:55 +0530

    Added

---
+++ p.py
@@ -0,0 +1,18 @@
+num = int(input("Enter Number: "))
+
+if num < 2:
+    print("Not Prime")
+
+else:
+    prime = True
+
+    for i in range(2, int(num**0.5)+1):
+
+        if num % i == 0:
+            prime = False
+            break
+
+    if prime:
+        print("Prime")
+    else:
+        print("Not Prime")
